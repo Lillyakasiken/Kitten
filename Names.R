f31c0672795d46b760430122d62c2eb638463db3
李瘦瘦 <lishoushou@lishoushoudeMacBook-Pro.local>
2022-11-06 18:34:42 +0100

    update

--- Names.R
+++ Names.R
@@ -5,7 +5,7 @@
 rm(list = ls())
 
 # use birthday as seed
-set.seed(14102022)
+set.seed(20220929)
 namebase <- c("buckbeak", "dobby", "多米", "旺仔", "狗蛋", "kiwi", "puppy", 
               "caesar", "chiot", "porcin", "仙仙")
 sample(namebase, 1)
